4b8551f4f2fac028c0dac5e74a1c14ad0fb251a3
ivanwakeup <ivan9miller@gmail.com>
2017-01-22 23:01:26 -0700

    yep

--- src/main/scala/Tester.scala
+++ src/main/scala/Tester.scala
@@ -1,6 +1,7 @@
 /**
   * Created by developer on 1/18/17.
   */
+import transform.ColumnTransformations
 object Tester {
 
   def main(args: Array[String]): Unit = {
@@ -9,6 +10,11 @@ object Tester {
       def apply(): java.util.Date = new java.util.Date()
     }
 
-    println(Test())
+    val t = ColumnTransformations
+
+    val strs = Array("5944", "50000", "543", "403334")
+
+    val out = strs.map(ele => t.icd9Format(ele))
+
   }
 }
--- src/main/scala/transform/ColumnTransformations.scala
+++ src/main/scala/transform/ColumnTransformations.scala
@@ -3,6 +3,7 @@ package transform
 import java.text.SimpleDateFormat
 import org.apache.spark.sql.functions.udf
 import org.apache.spark.sql._
+import scala.collection.immutable.StringOps
 
 /**
   * Created by developer on 1/17/17.
@@ -14,8 +15,8 @@ object ColumnTransformations {
   val stripCommas = udf((str: String) => str.replaceAll(",", ""))
 
   //define date input/output formats
-  val formatStr = udf((str: String) => {
-    val inputFormat = new SimpleDateFormat("m-d-yyyy")
+  val formatStr = udf((str: String, format: String) => {
+    val inputFormat = new SimpleDateFormat(format)
     val outputFormat = new SimpleDateFormat("mm-dd-yyyy")
     outputFormat.format(inputFormat.parse(str))
   })
@@ -28,4 +29,22 @@ object ColumnTransformations {
     str.replaceAll("\\.", "")
   })
 
+  val icd9Format = udf((str: String) => {
+    if(str.trim.length == 5)
+      str
+    else str.padTo(5,"0").mkString
+  })
+
+  val cleanZip = udf((str: String) => {
+    val regex = "^[0-9]{5}(?:-[0-9]{4})?$"
+    if(str.matches(regex))
+      str
+    else str.trim.replaceAll("[^\\p{L}\\p{Nd}]+", "")
+  })
+
+  val replaceSpecial = udf((str: String) => {
+    str.trim.replaceAll("[^\\p{L}\\p{Nd}]+", "")
+  })
+
+  
 }
